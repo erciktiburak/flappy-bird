f146e8a6f366fc1f9d2c46acf526d871ba7df94b
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2024-02-20 16:07:25 +0300

    Create the start menu for the Flappy Bird game

---
+++ FlappyBirdMenu.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FlappyBirdMenu extends JFrame {
+
+    public FlappyBirdMenu() {
+        setTitle("Flappy Bird");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JButton startButton = new JButton("Start Game");
+        startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Start the game
+            }
+        });
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        panel.add(Box.createVerticalGlue());
+        panel.add(startButton);
+        panel.add(Box.createVerticalStrut(20));
+        panel.add(exitButton);
+        panel.add(Box.createVerticalGlue());
+
+        add(panel);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FlappyBirdMenu();
+    }
+}
